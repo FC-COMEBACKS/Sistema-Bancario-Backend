8ad73ea683eda8b0cfa1637ad3ddaeccac02ce05
PXXxBADBOYxXXP <marcojosepro2007@gmail.com>
2025-06-20 17:21:04 -0600

    implementacion admin default

--- configs/adminDefault.js
+++ configs/adminDefault.js
@@ -3,7 +3,7 @@ import Usuario from "../src/user/user.model.js";
 
 export const crearAdmin = async () => {
     try {
-        const adminExistente = await Usuario.findOne({ rol: "ADMIN" });
+        const adminExistente = await Usuario.findOne({ username: "ADMINB" });
 
         if (adminExistente) {
             console.log("Ya existe un administrador en la base de datos.");
@@ -12,11 +12,16 @@ export const crearAdmin = async () => {
 
         const datosAdmin = {
             username: "ADMINB",
-            password: "ADMINB",
+            password: await hash("ADMINB"),
+            nombre: "Marco",
+            email: "adminb@admin.com",
+            celular: "37996330",
+            direccion: "Oficina Central",
+            dpi: "7030304051234",
+            nombreTrabajo: "Administrador",
+            ingresosMensuales: 10000,
+            rol: "ADMIN"
         };
-
-        const encryptedPassword = await hash(datosAdmin.password);
-        datosAdmin.password = encryptedPassword;
 
         const nuevoAdmin = new Usuario(datosAdmin);
         await nuevoAdmin.save();
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import {config} from "dotenv";
 import { initServer } from "./configs/server.js";
+import { crearAdmin} from "./configs/adminDefault.js"
 
 config();
 initServer();
+crearAdmin()
