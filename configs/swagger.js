644e57154ce6a4387fb8275807f752871f721e76
MarcoMJ2023060 <marcojosepro2007@gmail.com>
2025-06-20 09:25:53 -0600

    servidor y configuracion basica

---
+++ configs/swagger.js
@@ -0,0 +1,30 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options ={
+    swaggerDefinition:{
+        openapi:"3.0.0",
+        info:{
+            title: "Hacker Rank Bank API",
+            version: "1.0.0",
+            description: "API para un sistema bancario",
+            contact:{
+                name: "FCComebacks",
+                email: "FCComebacks@gmail.com"
+            }
+        },
+        servers:[
+            {
+                url: "http://127.0.0.1:3000/HRB/v1"
+            }
+        ]
+    },
+    apis:[
+        "./src/auth/auth.routes.js",
+        "./src/user/user.routes.js",
+    ]
+}
+
+const swaggerDocs = swaggerJSDoc(options)
+
+export { swaggerDocs, swaggerUi}
